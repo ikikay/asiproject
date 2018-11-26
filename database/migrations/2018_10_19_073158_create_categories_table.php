beab95d17057da1e6f63f4b6d60a910392d98971
dedeheu <boudet.nicolas.pro@gmail.com>
2018-11-26 16:26:36 +0100

    test git nrv

--- database/migrations/2018_10_19_073158_create_categories_table.php
+++ database/migrations/2018_10_19_073158_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             
-            
+            //TODO
             
             $table->timestamps();
         });
