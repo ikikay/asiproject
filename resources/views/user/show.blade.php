a3b0d85aea1eb15e907806d5f35e7953dafa1729
dedeheu <boudet.nicolas.pro@gmail.com>
2019-04-10 15:34:47 +0200

    modification de la page user.show : 	- ajout de l'image du logo du lycée 	- ajout d'une balise div type alert qui ne disparait pas avec le message pour dire que le questionnaire a été fait ou pas
    modification de la function toDashboard avec l'ajout d'un bool et d'une requete sql pour savoir si l'utilisateur connecté apparait dans la table reponse
    et renvoi de la variable $bool sous le nom de $aRep dans la vu user.show

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -5,18 +5,30 @@
     <div class="container">
         <div class="row">
             <a class="navbar-brand" href="{{ route('dashboard') }}"></a>
-            <h1>Accueil - Profil de l'utilisateur</h1>   
+            <img src="{{url('img/logo_lycée.jpg') }}" />           
+        </div>
+        <div class="row">
+            @if ($aRep)
+            <div class="alert alert-success">
+                Le questionnaire a bien été fait !
+            </div>
+            @else
+            <div class="alert alert-danger">
+                vous n'avez pas fait le questionnaire !
+            </div>
+            @endif
+
+            <h1 class="text-center">Mon profile</h1>
         </div>
     </div>    
 </div>
 @stop
 
 @section('content')
-
 <div class="col-md-12">
     <div class="container">
         <div class="row">
-            
+
         </div>        
     </div>
 </div>
